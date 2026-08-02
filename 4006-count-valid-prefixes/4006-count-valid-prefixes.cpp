6e889aabb3b058b0087602072cfdd28b3984c82e
Ashwikha <ashwikhas@gmail.com>
2026-08-02 12:48:05 +0530

    Time: 0 ms (100%), Space: 9.4 MB (66.67%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int ans=0;
+        int o=0,z=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='0'){
+                o++;
+            }
+            else
+            {
+                z++;
+            }
+            if(abs(o-z)<=1){
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
